c6b15acc8a1352d743b5e68be8d7d4fe2a0e668e
Nathen Harvey <nharvey@chef.io>
2017-11-29 10:01:12 -0500

    Expose port, clean-up plan
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

--- complete/habitat/plan.sh
+++ complete/habitat/plan.sh
@@ -1,33 +1,10 @@
 pkg_name=gs-accessing-data-mysql
 pkg_origin=nathenharvey
 pkg_version="0.1.0"
-pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=('Apache-2.0')
+pkg_maintainer="Nathen Harvey <nharvey@chef.io>"
+pkg_license=("Apache-2.0")
 pkg_scaffolding="core/scaffolding-gradle"
-pkg_source="http://some_source_url/releases/${pkg_name}-${pkg_version}.tar.gz"
-# pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
-# pkg_shasum="TODO"
-# pkg_deps=(core/glibc)
-# pkg_build_deps=(core/make core/gcc)
-# pkg_lib_dirs=(lib)
-# pkg_include_dirs=(include)
-# pkg_bin_dirs=(bin)
-# pkg_pconfig_dirs=(lib/pconfig)
-# pkg_svc_run="bin/haproxy -f $pkg_svc_config_path/haproxy.conf"
-# pkg_exports=(
-#   [host]=srv.address
-#   [port]=srv.port
-#   [ssl-port]=srv.ssl.port
-# )
-# pkg_exposes=(port ssl-port)
-# pkg_binds=(
-#   [database]="port host"
-# )
-# pkg_binds_optional=(
-#   [storage]="port host"
-# )
-# pkg_interpreters=(bin/bash)
-# pkg_svc_user="hab"
-# pkg_svc_group="$pkg_svc_user"
-# pkg_description="Some description."
-# pkg_upstream_url="http://example.com/project-name"
+pkg_upstream_url="https://github.com/nathenharvey/gs-accessing-data-mysql/tree/master/complete"
+pkg_exports=(
+  [port]=port
+)
